c423eb4e83dd4e7c1c621f2eb066cdb7d246372c
Adam Horvath <adam@nethosting.hu>
2019-01-05 14:38:51 +1100

    Adding protected commands.

--- gdocs_latex_highlight.js
+++ gdocs_latex_highlight.js
@@ -1,33 +1,67 @@
 function latexify() {
-  var body = DocumentApp.getActiveDocument().getBody();
+  const gray_color = "#a8a8a8";
+  const note_marker = "%";
+  const command_marker = "\\";
+  const beginre = /\\begin{(\w+)}/;
+  const endre = /\\end{(\w+)}/;
 
-  var gray_color = "#a8a8a8";
-  var note_marker = "%";
-  var beginre = /\\begin{(\w+)}/;
-  var endre = /\\end{(\w+)}/;
+  const headingmappings = [
+    "\\section{",
+    DocumentApp.ParagraphHeading.HEADING1,
+    "\\subsection{",
+    DocumentApp.ParagraphHeading.HEADING2,
+    "\\subsubsection{",
+    DocumentApp.ParagraphHeading.HEADING3,
+    "\\paragraph{",
+    DocumentApp.ParagraphHeading.HEADING4
+  ];
 
-  var headingmappings = {
-    "\\section{": DocumentApp.ParagraphHeading.HEADING1,
-    "\\subsection{": DocumentApp.ParagraphHeading.HEADING2,
-    "\\subsubsection{": DocumentApp.ParagraphHeading.HEADING3,
-    "\\paragraph{": DocumentApp.ParagraphHeading.HEADING3
-  };
+  const protectedcommands = [
+    "abstract",
+    "abstracttext",
+    "affiliation",
+    "author",
+    "being{abstract}",
+    "journal",
+    "keywords",
+    "note",
+    "parencite",
+    "shorttitle",
+    "textbf",
+    "textbf",
+    "textcite",
+    "textit",
+    "textit",
+    "textmd",
+    "textrm",
+    "textsc",
+    "textsf",
+    "textsl",
+    "texttt",
+    "textup",
+    "title"
+  ];
 
-  var paragraphs = body.getParagraphs();
+  const body = DocumentApp.getActiveDocument().getBody();
+  const paragraphs = body.getParagraphs();
+
   var beginname = "";
   var endname = "";
 
   paragraphs.forEach(function(p) {
     var str = p.getText();
 
-    Object.keys(headingmappings).forEach(function(key) {
+    for (var i = 0; i < headingmappings.length; i += 2) {
+      var key = headingmappings[i];
+      var stylevalue = headingmappings[i + 1];
       if (str.indexOf(key) != 0) {
-        return;
+        continue;
       }
       var style = {};
-      style[DocumentApp.Attribute.HEADING] = headingmappings[key];
+      style[DocumentApp.Attribute.HEADING] = stylevalue;
       p.setAttributes(style);
-    });
+      return;
+    }
 
     if (!beginname) {
       var beginmatches = beginre.exec(str);
@@ -41,7 +75,19 @@ function latexify() {
 
     var note = str.indexOf(note_marker) == 0;
 
-    if (beginname || note) {
+    var command = false;
+    if (str.indexOf(command_marker) == 0) {
+      command = true;
+      for (var i = 0; i < protectedcommands.length; i++) {
+        var pc = protectedcommands[i];
+        if (str.indexOf(pc) == 1) {
+          command = false;
+          break;
+        }
+      }
+    }
+
+    if (beginname || note || command) {
       var style = {};
       style[DocumentApp.Attribute.FOREGROUND_COLOR] = gray_color;
       p.setAttributes(style);
@@ -61,7 +107,7 @@ function latexify() {
 }
 
 function onOpen() {
-  var ui = DocumentApp.getUi();
-  var menu = ui.createAddonMenu();
+  const ui = DocumentApp.getUi();
+  const menu = ui.createAddonMenu();
   menu.addItem("Latex Syntax Highlight", "latexify").addToUi();
 }
